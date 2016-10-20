683125fdba950a790a851b71b7a2293573754bef
Nicolò Vescera <nicolo.vescera@gmail.com>
2016-10-20 11:20:34 +0200

    Init project

---
+++ src/assassination_airport/Assassination_Airport.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assassination_airport;
+
+/**
+ *
+ * @author Studenti
+ */
+public class Assassination_Airport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
