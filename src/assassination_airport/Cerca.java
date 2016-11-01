0e3239d5b641a0c530144bce1abec46b93b63dfd
Nicolò Vescera <nicolo.vescera@gmail.com>
2016-11-01 18:56:20 +0100

    Commentato codice

--- src/assassination_airport/Cerca.java
+++ src/assassination_airport/Cerca.java
@@ -15,21 +15,22 @@ import javax.swing.JOptionPane;
  */
 public class Cerca {
     static public void codice(Map<String, Nazione>nazioni, String codice){
+        //dichiarazione ed inizializzazione dell'arrylist che conterrà i risultati
         ArrayList<ArrayList> result = new ArrayList();
         
+        //lambda expression per accedere a tutti gli aeroporti di tutte le nazioni
         nazioni.forEach((String a, Nazione n) -> {
             n.aeroporti.forEach((String key, Aeroporto aeroporto) -> {
                 result.add(aeroporto.cercaPerCodice(codice));
             });
         });
         
+        //stampa dei risultati tramite JOptionPane
         String stampa = "";
         for(ArrayList elem:result){
             for(int i = 0; i < elem.size(); i++){
                 Volo temp = (Volo) elem.get(i++);
                 stampa += "Codice volo: "+temp.codice_volo+"\nCompagnia: "+temp.compagnia+"\nData: "+temp.data+"\nDestinazione: "+temp.destinazione+"\n-----------------------------\n";
-                //System.out.println("Codice volo: "+temp.codice_volo+" Compagnia: "+temp.compagnia+" Data: "+temp.data+" Destinazione: "+temp.destinazione);
-                //System.out.println("-----------------------------");
             }
         }
         
@@ -38,21 +39,23 @@ public class Cerca {
     }
     
     static public void destinazione(Map<String, Nazione>nazioni, String destinazione){
+        //dichiarazione ed inizializzazione dell'arrylist che conterrà i risultati
         ArrayList<ArrayList> result = new ArrayList();
         
+        //lambda expression per accedere a tutti gli aeroporti di tutte le nazioni
         nazioni.forEach((String a, Nazione n) -> {
             n.aeroporti.forEach((String key, Aeroporto aeroporto) -> {
+                //aggiunge i risultati della ricerca all'array creato in precedenza
                 result.add(aeroporto.cercaPerDestinazione(destinazione));
             });
         });
         
+        //stampa dei risultati tramite JOptionPane
         String stampa = "";
         for(ArrayList elem:result){
             for(int i = 0; i < elem.size(); i++){
                 Volo temp = (Volo) elem.get(i++);
                 stampa += "Codice volo: "+temp.codice_volo+"\nCompagnia: "+temp.compagnia+"\nData: "+temp.data+"\nDestinazione: "+temp.destinazione+"\n-----------------------------\n";
-                //System.out.println("Codice volo: "+temp.codice_volo+" Compagnia: "+temp.compagnia+" Data: "+temp.data+" Destinazione: "+temp.destinazione);
-                //System.out.println("-----------------------------");
             }
         }
         
