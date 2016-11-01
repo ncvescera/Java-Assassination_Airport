4aaefa6473e7a6498559680e3eeb981a54200be0
Nicolò Vescera <nicolo.vescera@gmail.com>
2016-11-01 17:44:28 +0100

    Aggiuna interfaccia grafica per la ricerca

--- src/assassination_airport/GUI.java
+++ src/assassination_airport/GUI.java
@@ -18,8 +18,13 @@ import javax.swing.JTextField;
  * @author Nicolò Vescera
  */
 public class GUI extends JFrame{
+    //Dichiarazione dei JPanels
     JPanel      top_insert;
     JPanel      insert;
+    //panel per la ricerca
+    JPanel      search;
+    JPanel      panel_search_codice;
+    JPanel      panel_search_destinazione;
     
     //Dichiarazione JLables
     JLabel      lab_nazioni;
@@ -28,7 +33,9 @@ public class GUI extends JFrame{
     JLabel      lab_destinazione;
     JLabel      lab_data;
     JLabel      lab_codice;
-    
+    //labels per la ricerca
+    JLabel      lab_search_codice;
+    JLabel      lab_search_destinazione;
     //Dichiarazioni JComboBoxes
     JComboBox   nazioni;
     JComboBox   aeroporto;
@@ -38,9 +45,14 @@ public class GUI extends JFrame{
     JTextField  destinazione;
     JTextField  data;
     JTextField  codice;
+    //textfields per la ricerca
+    JTextField  search_codice;
+    JTextField  search_destinazione;
     
     //Dichiarazione Buttons
     JButton     aggiungi;
+    JButton     btn_search_codice;
+    JButton     btn_search_destinazione;
   
     //Array associativo di nazioni che viene passato a questa classe
     Map<String, Nazione> assoc_nazioni;
@@ -49,36 +61,51 @@ public class GUI extends JFrame{
         this.assoc_nazioni = assoc_nazioni;
         
         //inizializzazione pennelli per inserimento dati
-        top_insert = new JPanel(new GridLayout(1,2));
-        insert = new JPanel(new GridLayout(6,2));
+        top_insert          = new JPanel(new GridLayout(1,2));
+        insert              = new JPanel(new GridLayout(6,2));
+        //panel per la ricerca
+        panel_search_codice         = new JPanel(new GridLayout(3,1));
+        panel_search_destinazione   = new JPanel(new GridLayout(3,1));
+        search                      = new JPanel(new GridLayout(1,2));
         
         //inizializazione label e combobox NAZIONE
-        lab_nazioni = new JLabel("Nazione: ");
-        nazioni = new JComboBox(Database.nazioni);
+        lab_nazioni     = new JLabel("Nazione: ");
+        nazioni         = new JComboBox(Database.nazioni);
         
         //inizializazione label e combobox AEROPORTO
-        lab_aeroporto = new JLabel("Aeroporto: ");
-        aeroporto = new JComboBox(Database.areoporto_ICE);
+        lab_aeroporto   = new JLabel("Aeroporto: ");
+        aeroporto       = new JComboBox(Database.areoporto_ICE);
         
         //inizializazione label e combobox COMPAGNIA AEREA
-        lab_compagnia = new JLabel("Compagnia aerea:");
-        compagnia = new JComboBox(Database.compagnie_ICE);
+        lab_compagnia   = new JLabel("Compagnia aerea:");
+        compagnia       = new JComboBox(Database.compagnie_ICE);
         
         //inizializazione label e textfield DESTINAZIONE
-        lab_destinazione = new JLabel("Destinazione: ");
-        destinazione = new JTextField(20);
+        lab_destinazione    = new JLabel("Destinazione: ");
+        destinazione        = new JTextField(20);
         
         //inizializazione label e textfield DATA
-        lab_data = new JLabel("Data: ");
-        data = new JTextField(20);
+        lab_data    = new JLabel("Data: ");
+        data        = new JTextField(20);
         
         //inizializazione label e textfield CODICE VOLO
-        lab_codice = new JLabel("Codice: ");
-        codice = new JTextField(20);
+        lab_codice  = new JLabel("Codice: ");
+        codice      = new JTextField(20);
         
-        //inizializzazione del bottono AGGIUNGI
+        //inizializzazione label e textfield CERCA CODICE
+        lab_search_codice       = new JLabel("Cerca per codice:");
+        search_codice           = new JTextField(20);
+        
+        lab_search_destinazione = new JLabel("Cerca per destinazione:");
+        search_destinazione     = new JTextField(20);
+        
+        //inizializzazione del bottone AGGIUNGI
         aggiungi = new JButton("Aggiungi !");
         aggiungi.setFont(new Font("Arial", Font.PLAIN, 40));
+        
+        //inizializzazione del bottone per la RICERCA CODICE
+        btn_search_codice       = new JButton("Cerca !");
+        btn_search_destinazione = new JButton("Cerca !");
         
         //aggiunta degli elementi al panel INSERT
         insert.add(lab_nazioni);
@@ -94,6 +121,16 @@ public class GUI extends JFrame{
         insert.add(lab_codice);
         insert.add(codice);
        
+        panel_search_codice.add(lab_search_codice);
+        panel_search_codice.add(search_codice);
+        panel_search_codice.add(btn_search_codice);
+        
+        panel_search_destinazione.add(lab_search_destinazione);
+        panel_search_destinazione.add(search_destinazione);
+        panel_search_destinazione.add(btn_search_destinazione);
+        //aggiunta gegli elementi al panel SEARCH
+        search.add(panel_search_codice);
+        search.add(panel_search_destinazione);
         //aggiunta del panel INSERT al panel TOP_INSERT
         top_insert.add(insert);
         top_insert.add(aggiungi);
@@ -103,10 +140,10 @@ public class GUI extends JFrame{
         
         //aggiunta del panle TOP_INSERT al FRAME
         this.add(top_insert,"North");
-        
+        this.add(search,"Center");
         //set dei valori principali del frame
         this.setVisible(true);
-        this.setSize(500,200);
+        this.setSize(700,250);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
@@ -152,6 +189,20 @@ public class GUI extends JFrame{
             assoc_nazioni.forEach((String s, Nazione a) -> {
                 a.print();
             });
+        });
+        
+        /*
+            ACTION LISTENER CERCA CODICE
+        */
+        btn_search_codice.addActionListener((ActionEvent e) -> {
+            //invoco metodo di ricerca
+        });
+        
+        /*
+            ACTION LISTENER CERCA DESTINAZIONE
+        */
+        btn_search_destinazione.addActionListener((ActionEvent e) -> {
+            //invoco metodo di ricerca
         });
     }
     
