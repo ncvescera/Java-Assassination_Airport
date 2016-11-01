e7bfcfc5b0ada358e2488680fa02b41310e5ad35
Nicolò Vescera <nicolo.vescera@gmail.com>
2016-11-01 17:57:58 +0100

    Creata classe Cerca

---
+++ src/assassination_airport/Cerca.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assassination_airport;
+
+import java.util.Map;
+
+/**
+ *
+ * @author ncvescera
+ */
+public class Cerca {
+    static public void codice(Map<String, Nazione>nazioni, String codice){
+        //da definire
+    }
+    
+    static public void destinazione(Map<String, Nazione>nazioni, String destinazione){
+        //da definire
+    }
+}
